a07836b21dccc071bde9ded6cb691c897a8521eb
patricia <patricia.selpa@gmail.com>
2020-07-20 20:11:40 -0300

    e

--- Program.cs
+++ Program.cs
@@ -6,81 +6,85 @@ namespace primeira_aula
 			{
 				static void Main(string[] args)
 				{
-						// 1:
-					// System.Console.WriteLine("Digite a letra correspondente a questão: ");
-					// var opcao = Console.ReadLine();
-					// if (opcao == "a"){
-					// 	for (int i = 1; i <= 10; i++){ 
-					// 		System.Console.WriteLine(i);
-					// 	}
-					// }
-					// if (opcao == "b"){
-					// 	for (int i = 10; i >= 1; i--){
-					// 		System.Console.WriteLine(i);
-					// 	}
-					// }
-					// if (opcao == "c"){
-					// 	for (int i = 1; i < 11; i++){
-					// 		if (i % 2 == 0){
-					// 		System.Console.WriteLine(i);
-					// 		}
-					// 	}
-					// }
+				// 		// 1:
+				// 	// System.Console.WriteLine("Digite a letra correspondente a questão: ");
+				// 	// var opcao = Console.ReadLine();
+				// 	// if (opcao == "a"){
+				// 	// 	for (int i = 1; i <= 10; i++){ 
+				// 	// 		System.Console.WriteLine(i);
+				// 	// 	}
+				// 	// }
+				// 	// if (opcao == "b"){
+				// 	// 	for (int i = 10; i >= 1; i--){
+				// 	// 		System.Console.WriteLine(i);
+				// 	// 	}
+				// 	// }
+				// 	// if (opcao == "c"){
+				// 	// 	for (int i = 1; i < 11; i++){
+				// 	// 		if (i % 2 == 0){
+				// 	// 		System.Console.WriteLine(i);
+				// 	// 		}
+				// 	// 	}
+				// 	// }
 					
-					// - - - - - - - - - - - - - - - -
+				// 	// - - - - - - - - - - - - - - - -
 
-						// 2:
+				// 		// 2:
 
-					// var conta = 0;
-					// for (int i = 1; i <= 100; i++){
-					// 	conta += i;
-					// }
-					// System.Console.WriteLine(conta);
+				// 	// var conta = 0;
+				// 	// for (int i = 1; i <= 100; i++){
+				// 	// 	conta += i;
+				// 	// }
+				// 	// System.Console.WriteLine(conta);
 					
-					// - - - - - - - - - -
+				// 	// - - - - - - - - - -
 					
-						// 3: 
-					// for (int i = 1; i < 200; i++){
-					// 	if(i % 2 == 1){
-					// 		System.Console.WriteLine(i);
-					// 	}
-					// }
+				// 		// 3: 
+				// 	// for (int i = 1; i < 200; i++){
+				// 	// 	if(i % 2 == 1){
+				// 	// 		System.Console.WriteLine(i);
+				// 	// 	}
+				// 	// }
 
-				// --------------
+				// // --------------
 
-					// 4:
+				// 	// 4:
 
-				// var counter = 0.0;
-				// var idade = 0.0;
+				// // var counter = 0.0;
+				// // var idade = 0.0;
 
-				// while(true){
-				// 	System.Console.WriteLine("Digite uma idade: ");
+				// // while(true){
+				// // 	System.Console.WriteLine("Digite uma idade: ");
+				// // 	var result = Console.ReadLine();
+				// // 	if (result == "0"){
+				// // 		break;
+				// // 	}
+				// // 	idade += double.Parse(result);
+				// // 	counter++;
+				// // }
+				// // System.Console.WriteLine(idade / counter);
+
+				// // - - - - - - - - - -
+
+				// 	// 5:
+				// var idade = new int[5];
+				// for(int cont = 0; cont < 5; cont++){
+				// 	System.Console.WriteLine("Digite sua idade: ");
 				// 	var result = Console.ReadLine();
-				// 	if (result == "0"){
-				// 		break;
-				// 	}
-				// 	idade += double.Parse(result);
-				// 	counter++;
+				// 	idade[cont] = Int32.Parse(result);
 				// }
-				// System.Console.WriteLine(idade / counter);
-
-				// - - - - - - - - - -
-
-					// 5:
-				var idade = new int[5];
-				for(int cont = 0; cont < 5; cont++){
-					System.Console.WriteLine("Digite sua idade: ");
-					var result = Console.ReadLine();
-					idade[cont] = Int32.Parse(result);
-				}
-				var counter = 0;
-				for (int i = 0; i < 5; i++){
-					if (idade[i] <= 35 && idade[i] >= 18){
-						counter++;
-					}
+				// var counter = 0;
+				// for (int i = 0; i < 5; i++){
+				// 	if (idade[i] <= 35 && idade[i] >= 18){
+				// 		counter++;
+				// 	}
 					
-				}
-				System.Console.WriteLine($"{counter * 20}% das mulheres estão na faixa entre 18 e 35 anos");
+				// }
+				// System.Console.WriteLine($"{counter * 20}% das mulheres estão na faixa entre 18 e 35 anos");
+
+				
+
+
 				}
 			}
 	}
