615e2919991fd0da0fc58e89b41daa4f17303e14
guilhermekuehl <guikuehlkeller120@gmail.com>
2020-07-28 15:38:53 -0300

    first commit

--- Program.cs
+++ Program.cs
@@ -85,75 +85,89 @@ namespace primeira_aula
 				// --------------------------
 
 					// 6:
-				string senha;
-				string name1;
-				string name2;
+			// 	string senha;
+			// 	string name1;
+			// 	string name2;
 
-				Console.WriteLine("Qual modo você quer? ");
-				var menu = Console.ReadLine();
+			// 	Console.WriteLine("Qual modo você quer? ");
+			// 	var menu = Console.ReadLine();
 
-				if (menu == "1"){
-				Console.WriteLine("Insira senha para você poder se cadastrar: ");
-				senha = Console.ReadLine();
-				if (senha == "Pa$$w0rd"){
-					Console.WriteLine("Informe seu nome: ");
-					name1 = Console.ReadLine();	
-				}
-				else{
-					Console.WriteLine("SENHA INCORRETA, próximo candidato");
-				}
+			// 	if (menu == "1"){
+			// 	Console.WriteLine("Insira senha para você poder se cadastrar: ");
+			// 	senha = Console.ReadLine();
+			// 	if (senha == "Pa$$w0rd"){
+			// 		Console.WriteLine("Informe seu nome: ");
+			// 		name1 = Console.ReadLine();	
+			// 	}
+			// 	else{
+			// 		Console.WriteLine("SENHA INCORRETA, próximo candidato");
+			// 	}
 
-				Console.WriteLine("Insira senha para você poder se cadastrar: ");
-				senha = Console.ReadLine();
-				if (senha == "Pa$$w0rd"){
-					Console.WriteLine("Informe seu nome: ");
-					name2 = Console.ReadLine();
-				}
-				else {
-					Console.WriteLine("SENHA INCORRETA, PRÓXIMO MODO");
-			}
-				}
-				if (menu == "2"){
-					while(true){
-						System.Console.WriteLine("Qual candidato você quer votar? ");
-						var vote = Console.ReadLine();
-						int vote1 = 0;
-						int vote2 = 0;
-						if (vote == "1"){
-            	vote1++;
-						}
-						else if (vote == "2"){
-							vote2++;
-						}
-						else{
-							continue;
-						}
-						if (vote1 > vote2){
-							System.Console.WriteLine("O candidato 1 ganhou, com o total de ",vote1, "votos");
-						}
-						else if (vote2 > vote1){
-							System.Console.WriteLine("O candidato 2 ganhou, com o total de ", vote2, "votos");
-						}
-						else{
-							System.Console.WriteLine("SEGUNDO TURNO");
-							if (vote == "1"){
-                vote1++;
-						}
-						else if (vote == "2"){
-							vote2++;
-						}
-						else{
-							continue;
-						}
-						if (vote1 > vote2){
-							System.Console.WriteLine("O candidato 1 ganhou, com o total de ", vote1, "votos");
-					}
-						else if (vote2 > vote1){
-							System.Console.WriteLine("O candidato 2 ganhou, com o total de ", vote2, "votos");
-						}
-						}
-				}
-			}
+			// 	Console.WriteLine("Insira senha para você poder se cadastrar: ");
+			// 	senha = Console.ReadLine();
+			// 	if (senha == "Pa$$w0rd"){
+			// 		Console.WriteLine("Informe seu nome: ");
+			// 		name2 = Console.ReadLine();
+			// 	}
+			// 	else {
+			// 		Console.WriteLine("SENHA INCORRETA, PRÓXIMO MODO");
+			// }
+			// 	}
+			// 	if (menu == "2"){
+			// 		while(true){
+			// 			System.Console.WriteLine("Qual candidato você quer votar? ");
+			// 			var vote = Console.ReadLine();
+			// 			int vote1 = 0;
+			// 			int vote2 = 0;
+			// 			if (vote == "1"){
+            // 	vote1++;
+			// 			}
+			// 			else if (vote == "2"){
+			// 				vote2++;
+			// 			}
+			// 			else{
+			// 				continue;
+			// 			}
+			// 			if (vote1 > vote2){
+			// 				System.Console.WriteLine("O candidato 1 ganhou, com o total de ",vote1, "votos");
+			// 			}
+			// 			else if (vote2 > vote1){
+			// 				System.Console.WriteLine("O candidato 2 ganhou, com o total de ", vote2, "votos");
+			// 			}
+			// 			else{
+			// 				System.Console.WriteLine("SEGUNDO TURNO");
+			// 				if (vote == "1"){
+            //     			vote1++;
+			// 			}
+			// 				else if (vote == "2"){
+			// 					vote2++;
+			// 			}
+			// 				else{
+			// 					continue;
+			// 			}
+			// 				if (vote1 > vote2){
+			// 					System.Console.WriteLine("O candidato 1 ganhou, com o total de ", vote1, "votos");
+			// 		}
+			// 				else if (vote2 > vote1){
+			// 					System.Console.WriteLine("O candidato 2 ganhou, com o total de ", vote2, "votos");
+			// 				}
+			// 			}
+			// 	}
+			// }
+			
+			// ----------------------------------------
+
+			// 7:
+			System.Console.WriteLine("Há quantos anos você fuma?");
+			var anos = Convert.ToInt32(Console.ReadLine());
+			System.Console.WriteLine("Quantos cigarros você fuma por dia?");
+			var nCigarros = Convert.ToInt32(Console.ReadLine());
+			System.Console.WriteLine("Qual o preço de uma carteira?");
+			var preco = Convert.ToDouble(Console.ReadLine());
+			var cigarro = preco / 20;
+			var tempo = anos * 365;
+			var result = cigarro * tempo;
+			System.Console.WriteLine(result);
 		}
 	}
 }
