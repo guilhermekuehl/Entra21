a0e69457ba6b1f1b49bb0e316bba7e56e9db2e98
guilhermekuehl <guikuehlkeller120@gmail.com>
2020-08-07 21:14:20 -0300

    add exercises

--- Arrays2.cs
+++ Arrays2.cs
@@ -12,10 +12,13 @@ namespace primeira_aula
             var array2 = new int[arraysLength];
             var temporary = new int[arraysLength];
 
+            System.Console.WriteLine("Digite os números do array1: ");
+
             for (int i = 0; i < arraysLength; i++)
             {
                 array1[i] = Int32.Parse(Console.ReadLine());
             }
+            System.Console.WriteLine("Digite ps números do array2");
 
             for (int i = 0; i < arraysLength; i++)
             {
