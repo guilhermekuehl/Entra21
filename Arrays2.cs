92436fb34bff9c5dce8412907e10d28980183330
guilhermekuehl <guikuehlkeller120@gmail.com>
2020-08-10 14:28:08 -0300

    add exercises

--- Arrays2.cs
+++ Arrays2.cs
@@ -16,19 +16,94 @@ namespace primeira_aula
 
             for (int i = 0; i < arraysLength; i++)
             {
+                try{
                 array1[i] = Int32.Parse(Console.ReadLine());
+                }
+                catch(System.Exception){
+                    System.Console.WriteLine("Não é possível inserir um valor deste tipo");
+                }
             }
-            System.Console.WriteLine("Digite ps números do array2");
+            System.Console.WriteLine("Digite os números do array2: ");
 
             for (int i = 0; i < arraysLength; i++)
             {
+                try{
                 array2[i] = Int32.Parse(Console.ReadLine());
+                }
+                catch(System.Exception){
+                    System.Console.WriteLine("Não é possível inserir um valor deste tipo");
+                }
             }
-            System.Console.WriteLine(string.Join(",", array1, array2));
+            System.Console.WriteLine("Arrays como o usuário informou: ");
+            for (int i = 0; i < arraysLength; i++)
+            {
+                System.Console.WriteLine($"Array1: {array1[i]}");
+            }
+
+            System.Console.WriteLine("");
+            
+            for (int i = 0; i < arraysLength; i++)
+            {
+                System.Console.WriteLine($"Array2: {array2[i]}");
+            }
+            System.Console.WriteLine("");
+            System.Console.WriteLine("Arrays trocados: ");
+            System.Console.WriteLine("");
+            for (int i = 0; i < arraysLength; i++)
+            {
+                temporary[i] = array1[i];
+                array1[i] = array2[i];
+                array2[i] = temporary[i];           
+            }
+
+            for (int i = 0; i < arraysLength; i++)
+            {
+                System.Console.WriteLine($"Array1: {array1[i]}");
+            }
+
+            System.Console.WriteLine("");
+            
+            for (int i = 0; i < arraysLength; i++)
+            {
+                System.Console.WriteLine($"Array2: {array2[i]}");
+            }
+
+        }
+        
+        static void Exercise2()
+        {
+            const int arrayLength = 3;
+            
+            var arrayOfNumbers = new double[arrayLength];
+
+            System.Console.WriteLine("Informe os números: ");
+
+            for (int i = 0; i < arrayLength; i++)
+            {
+                try
+                {
+                    arrayOfNumbers[i] = double.Parse(Console.ReadLine());
+                }
+                catch (System.Exception)
+                {
+                    System.Console.WriteLine("Insira apenas números, tente novamente!");
+                }                
+            }
+            foreach (var item in arrayOfNumbers)
+            {
+                if (item == item)
+                {
+                    System.Console.WriteLine("equals");
+                }
+                else
+                {
+                    System.Console.WriteLine("not equals");
+                }
+            }
         }
         static void Main(string[] args)
         {
-            Exercise1();
+            Exercise2();
         }
     }
 }
